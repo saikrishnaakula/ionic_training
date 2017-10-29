6b35c47f419b4e17be2d6fc8dd3c02d4e3c1b585
Sai Krishna Akula <saikrish.200@gmail.com>
2017-10-29 19:32:23 +0530

    v2 - inital commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SQLite,SQLiteObject  } from '@ionic-native/sqlite';
 import { Platform } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -9,10 +10,8 @@ import { Platform } from 'ionic-angular';
 })
 export class HomePage {
   name = <any>[];
-  // name.fname=[];
-  // name.lname=[];
   people =[];
-  constructor(public platform: Platform,public navCtrl: NavController,private sqlite: SQLite) {
+  constructor(public platform: Platform,public navCtrl: NavController,private sqlite: SQLite,public alertCtrl: AlertController) {
     this.platform.ready().then(() => {
             this.sqlite.create({name: "test.db", location: "default"}).then(() => {
                 this.refresh();
@@ -22,22 +21,35 @@ export class HomePage {
         });
   }
   public saveName(){
-    var sql = "INSERT INTO people (firstname, lastname) VALUES (?,?)";
+    var sql = "";
+    var param =[];
+
+    if(this.name.fname.length !=0 && this.name.lname.length !=0){
+    sql = "INSERT INTO people (firstname, lastname) VALUES (?,?)";
+    param = [this.name.fname, this.name.lname];
+  } else if(this.name.fname.length!=0 && this.name.lname.length ==0){
+    sql = "INSERT INTO people (firstname) VALUES (?)";
+    param = [this.name.fname];
+  } else if(this.name.fname.length ==0 && this.name.lname.length !=0){
+    sql = "INSERT INTO people (lastname) VALUES (?)";
+    param = [this.name.lname];
+  } else{
+    return;
+  }
     this.sqlite.create({
       name: 'test.db',
       location: 'default'
     })
     .then((db: SQLiteObject) => {
-      db.executeSql(sql, [this.name.fname, this.name.lname])
-      .then(() => {this.refresh();})
+      db.executeSql(sql, param)
+      .then(() => {
+        this.refresh();
+        this.name.fname ="";
+        this.name.lname="";
+      })
       .catch(e => console.log(e));
     })
   .catch(e => console.log(e));
-    // this.database.executeSql(sql, [this.name.fname, this.name.lname]).then((data) => {
-    //             console.log("INSERTED: " + JSON.stringify(data));
-    //         }, (error) => {
-    //             console.log("ERROR: " + JSON.stringify(error.err));
-    //         });
   }
   public refresh() {
     this.sqlite.create({
@@ -50,22 +62,86 @@ export class HomePage {
         this.people = [];
         if(data.rows.length > 0) {
           for(var i = 0; i < data.rows.length; i++) {
-            this.people.push({firstname: data.rows.item(i).firstname, lastname: data.rows.item(i).lastname});
+            this.people.push({firstname: data.rows.item(i).firstname, lastname: data.rows.item(i).lastname,id :data.rows.item(i).id});
           }
         }
       })
       .catch(e => console.log(e));
     })
     .catch(e => console.log(e));
-    //  this.database.executeSql("SELECT * FROM people", []).then((data) => {
-    //      this.people = [];
-    //      if(data.rows.length > 0) {
-    //          for(var i = 0; i < data.rows.length; i++) {
-    //              this.people.push({firstname: data.rows.item(i).firstname, lastname: data.rows.item(i).lastname});
-    //          }
-    //      }
-    //  }, (error) => {
-    //      console.log("ERROR: " + JSON.stringify(error));
-    //  });
   }
+  public rmName(id){
+    this.sqlite.create({
+      name: 'test.db',
+      location: 'default'
+    })
+    .then((db: SQLiteObject) => {
+      var sql = "delete FROM people where id =(?)"
+      db.executeSql(sql, [id])
+      .then((data) => {
+        this.refresh();
+      })
+      .catch(e => console.log(e));
+    })
+    .catch(e => console.log(e));
+  }
+
+
+
+public  editName(id) {
+  let prompt = this.alertCtrl.create({
+    title: 'Edit Name',
+    message: "Enter the new name",
+    inputs: [
+      {
+        name: 'fname',
+        placeholder: 'firstname'
+      },
+      {
+        name: 'lname',
+        placeholder: 'lastname'
+      }
+    ],
+    buttons: [
+      {
+        text: 'Cancel',
+        handler: data => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Save',
+        handler: data => {
+          this.sqlite.create({
+            name: 'test.db',
+            location: 'default'
+          })
+          .then((db: SQLiteObject) => {
+            var sql = "";
+            var param =[];
+            if(data.fname.length !=0 && data.lname.length !=0){
+            sql = "update people set firstname = (?), lastname =(?) where id =(?)";
+            param = [data.fname,data.lname,id];
+          } else if(data.fname.length!=0&& data.lname.length ==0){
+            sql = "update people set firstname = (?) where id =(?)";
+            param = [data.fname,id];
+          } else if(data.fname.length ==0 && data.lname.length !=0){
+            sql = "update people set lastname =(?) where id =(?)";
+            param = [data.lname,id];
+          }
+            db.executeSql(sql, param)
+            .then((data) => {
+              this.refresh();
+            })
+            .catch(e => console.log(e));
+          })
+          .catch(e => console.log(e));
+        }
+      }
+    ]
+  });
+  prompt.present();
+}
+
+
 }
